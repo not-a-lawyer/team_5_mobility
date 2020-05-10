923f1555170a102b2f7ffa8169d024ab7c186150
Maximilian Kroker <kroker@not-a.lawyer>
2020-05-10 14:11:18 +0200

    added compute_emmission function

--- python_logic.py
+++ python_logic.py
@@ -1,5 +1,25 @@
 from dateutil import parser
 
+def compute_emissions(transportation_types: list, transportation_length: list):
+    co2_emission = 0
+
+    co2_emission_dict = {'car':140,
+                         'bike': 0.3,
+                         'transit': 65,
+                         'pedestrian': 0}
+
+    if len(transportation_types) == len(transportation_length):
+        i = 0
+        for type in transportation_types:
+            co2_emission += co2_emission_dict[type]*transportation_length[i]
+            i +=1
+        return co2_emission
+
+    else:
+        raise Exception('Lists are not the same length')
+
+
+
 def compute_overall_trip_time(modal_data_request):
     """
     looks at the first and last time of the intermodal route, computing overall trip time
